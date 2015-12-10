64c202110c89e5f3b48f78648baf5949a85f6f0c
priyachand <priyachandjava@gmail.com>
2015-12-09 21:31:16 -0600

    Update test.js

--- debounce/test.js
+++ debounce/test.js
@@ -67,3 +67,25 @@ describe('debounce', function() {
 
 
 });
+
+Function.implement({
+	debounce: function(wait, immediate) {
+		var timeout, 
+		    func = this;
+		return function() {
+			var context = this, args = arguments;
+			var later = function() {
+				timeout = null;
+				if (!immediate) func.apply(context, args);
+			};
+			var callNow = immediate && !timeout;
+			clearTimeout(timeout);
+			timeout = setTimeout(later, wait);
+			if (callNow) func.apply(context, args);
+		};
+	}
+});
+
+// Use it!
+window.addEvent("resize", myFn.debounce(500));
+
