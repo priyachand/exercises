b73b3c7ec06dabbe80f9f119bba37ddc42a10be8
priyachand <priyachandjava@gmail.com>
2015-12-09 18:29:15 -0600

    Update test.js

--- flatten/test.js
+++ flatten/test.js
@@ -16,3 +16,21 @@ describe('flatten', function() {
     assert.deepEqual(flatten(arr), [1, 2, 3, 4, 5]);
   });
 });
+
+var flatten = function(toFlatten) {
+	  var isArray = Object.prototype.toString.call(toFlatten) === '[object Array]';
+
+	  if (isArray && toFlatten.length > 0) {
+	    var head = toFlatten[0];
+	    var tail = toFlatten.slice(1);
+
+	    return flatten(head).concat(flatten(tail));
+	  } else {
+	    return [].concat(toFlatten);
+	  }
+	};
+	
+	function testFlat()
+	{
+		alert(flatten([1,[2,3],4,[[5,6],7]]));
+	}
