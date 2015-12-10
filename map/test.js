90f2fc9dd80d3a71d0cf3cc43a845d8574938135
priyachand <priyachandjava@gmail.com>
2015-12-09 21:42:13 -0600

    Update test.js

--- map/test.js
+++ map/test.js
@@ -81,3 +81,11 @@ describe('map', function() {
     assert.equal(ctx, 3);
   });
 });
+
+function mymap(){
+		var numbers = [1, 4, 9];
+		var doubles = numbers.map(function(num) {
+		  return num * 2;
+		});
+		alert(doubles);
+	}
